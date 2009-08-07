345d2797febe6e7f6b600747545e7d2937cb75cc
aminmc <aminmc@5b3abbcc-8194-11de-91da-3d9a54079ac5>
2009-08-07 18:54:49 +0000

    removed a whole bunch of unnecessary code related to recursive directory look up. using apache commons FileUitls to do this function. updated test case to demonstrate that only files are returned from splitter.

--- trunk/spring-integration-lucene/src/main/java/com/amin/app/index/integration/splitter/URISplitter.java
+++ trunk/spring-integration-lucene/src/main/java/com/amin/app/index/integration/splitter/URISplitter.java
@@ -1,13 +1,12 @@
 package com.amin.app.index.integration.splitter;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.Collection;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 
 public class URISplitter {
@@ -22,51 +21,14 @@ public class URISplitter {
 			throw new IllegalArgumentException("uri does not exist");
 		}
 		if (fileToBeIndexed.isDirectory()) {
-			List<File> fileListing = getFileListing(fileToBeIndexed);
-			for (File file : fileListing) {
+			Collection<?> collectionOfFiles = FileUtils.listFiles(fileToBeIndexed, null, true);
+			for (Object fileObj : collectionOfFiles) {
+				File file = (File)fileObj;
 				listOfUris.add(file.toURI());
 			}
 		} else {
 			listOfUris.add(fileToBeIndexed.toURI());
 		}
 		return listOfUris;
-	}
-
-	private List<File> getFileListing(File aStartingDir) throws FileNotFoundException {
-		validateDirectory(aStartingDir);
-		List<File> result = getFileListingNoSort(aStartingDir);
-		Collections.sort(result);
-		return result;
-	}
-
-	private List<File> getFileListingNoSort(File aStartingDir) throws FileNotFoundException {
-		List<File> result = new ArrayList<File>();
-		File[] filesAndDirs = aStartingDir.listFiles();
-		List<File> filesDirs = Arrays.asList(filesAndDirs);
-		for (File file : filesDirs) {
-			if (!file.isDirectory()) {
-				result.add(file);
-			}
-			if (!file.isFile()) {
-				List<File> deeperList = getFileListingNoSort(file);
-				result.addAll(deeperList);
-			}
-		}
-		return result;
-	}
-
-	private void validateDirectory(File aDirectory) throws FileNotFoundException {
-		if (aDirectory == null) {
-			throw new IllegalArgumentException("Directory should not be null.");
-		}
-		if (!aDirectory.exists()) {
-			throw new FileNotFoundException("Directory does not exist: "+ aDirectory);
-		}
-		if (!aDirectory.isDirectory()) {
-			throw new IllegalArgumentException("Is not a directory: "+ aDirectory);
-		}
-		if (!aDirectory.canRead()) {
-			throw new IllegalArgumentException("Directory cannot be read: "+ aDirectory);
-		}
 	}
 }
